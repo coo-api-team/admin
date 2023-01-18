4d8cf36e5d26d24eafc6a3f369cba0edb3325550
LAPTOP-SL0JNDB7\NAMJAEMIN <nionu0430@gmail.com>
2023-01-18 19:52:51 +0900

    fix: 사용자 로그인 관련 소스 수정
    cors 관련 소스 수정
    OAuth resource server 관련 설정 추가
    OAuth 관련 미사용 소스 제거

--- spring/src/main/java/com/coocon/admin/config/SpringSecurityConfig.java
+++ spring/src/main/java/com/coocon/admin/config/SpringSecurityConfig.java
@@ -2,21 +2,38 @@ package com.coocon.admin.config;
 
 import com.coocon.admin.oauth.CooconOAuth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@EnableWebSecurity
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
 
@@ -33,19 +50,15 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                //.exceptionHandling()
-                //.authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                //.accessDeniedHandler(jwtAccessDeniedHandler)
-                .headers()
-                .frameOptions()
-                .sameOrigin();
+        http   .csrf().disable() //browser로부터 직접 받는게 아닐경우
+                .headers().frameOptions().sameOrigin()
+                .and().logout().logoutSuccessUrl("/");
+        http.cors();
 
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http    .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); //세션으로 진행하지 않음
 
-        http
-                .authorizeRequests()
+        http    .authorizeRequests()
                 .antMatchers("/","/signUp","/access-denied","/exception/**").permitAll()
                 .antMatchers("/oauth2/*").permitAll()
                 .antMatchers( "/error").permitAll() //정적리소스에 대한 접속 허용
@@ -54,47 +67,89 @@ public class SpringSecurityConfig {
                 .anyRequest().authenticated(); //permit한 리소스 제외 접근 시 인증 필요
 
         http.oauth2Login()
-                .userInfoEndpoint().userService(cooconOAuth2UserService);
+                .loginPage("http://localhost:3000/pages/login/login3")
+                .userInfoEndpoint().userService(cooconOAuth2UserService); //로그인 성공시 서비스
 
-                /*
-                .userInfoEndpoint().userService(cooconUserDetailsService)
-                .and()
-                .successHandler(configSuccessHandler())
-                .failureHandler(configFailureHandler())
-                .permitAll();
-*/
-        //.and()
-
-        //.and()
-        //.formLogin().loginPage("/auth/signIn") //설정해두면 로그인 없는 상태로 들어가면 이 페이지로 redirect 시킨다.
-        //.defaultSuccessUrl("/"); //로그인 성공 후 기본
-
-
-        //.and()
-        //.apply(new JwtSecurityConfig(tokenProvider));
         return http.build();
     }
 
+
     @Bean
-    public ClientRegistrationRepository clientRegistrationRepository(){
-        return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod(HttpMethod.POST);
+        corsConfiguration.addAllowedMethod(HttpMethod.GET);
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return source;
     }
 
-    private ClientRegistration googleClientRegistration() {
-        return ClientRegistration.withRegistrationId("google")
-                .clientId("google-client-id")
-                .clientSecret("google-client-secret")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
-                .scope("openid", "profile", "email", "address", "phone")
-                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
-                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
-                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
-                .userNameAttributeName(IdTokenClaimNames.SUB)
-                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
-                .clientName("Google")
-                .build();
+    List<String> clients = Arrays.asList("google","coocon","facebook");
+    /*
+        provider에 대한 초기 설정 bean
+     */
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository(){
+        List<ClientRegistration> registrations = clients.stream()
+                .map(client -> getRegistration(client))
+                .filter(registration -> registration != null)
+                .collect(Collectors.toList());
+
+        return new InMemoryClientRegistrationRepository(registrations);
+    }
+
+    private static String CLIENT_PROPERTY_KEY = "spring.security.oauth2.client.registration.";
+    private ClientRegistration getRegistration(String client) {
+        String clientId = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-id");
+
+        if (clientId == null) {
+            return null;
+        }
+
+        String clientSecret = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-secret");
+        if (client.equals("google")) {
+            return CommonOAuth2Provider.GOOGLE.getBuilder(client)
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .build();
+        }
+        if (client.equals("facebook")) {
+            return CommonOAuth2Provider.FACEBOOK.getBuilder(client)
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .build();
+        }
+        if(client.equals("coocon")){
+            return getCooconRegistration().clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .build();
+        }
+        return null;
+    }
+    @Autowired
+    private Environment env;
+
+    public ClientRegistration.Builder getCooconRegistration(){
+        ClientRegistration.Builder builder = ClientRegistration.withRegistrationId("coocon");
+
+        builder.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC);
+        builder.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE);
+        builder.redirectUri("{baseUrl}/{action}/oauth2/code/{registrationId}");//DEFAULT_REDIRECT_URL
+        builder.authorizationUri("http://localhost:8180/oauth2/v2/auth");
+        builder.tokenUri("http://localhost:8180/oauth2/v2/token");
+        builder.jwkSetUri("http://localhost:8180/oauth2/v2/certs");
+        builder.issuerUri("http://localhost:8180");
+        builder.userInfoUri("http://localhost:8180/oauth2/v2/userinfo");
+        builder.userNameAttributeName("id");
+        builder.scope("openid", "profile", "dashboard");
+        builder.userNameAttributeName(IdTokenClaimNames.SUB);
+        builder.clientName("Coocon");
+        return builder;
     }
 
 }
